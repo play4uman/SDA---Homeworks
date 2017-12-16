b806b78e3e9c06752c4e9fc99cb86470e3e6d67c
play4uman <33103332+play4uman@users.noreply.github.com>
2017-12-16 17:07:48 +0200

    Now works.

--- Shooting stars/Zadacha 5.cpp
+++ Shooting stars/Zadacha 5.cpp
@@ -1,9 +1,13 @@
-﻿// Zadacha 5.cpp : Defines the entry point for the console application.
+// Zadacha 5.cpp : Defines the entry point for the console application.
 //
 
 #include "stdafx.h"
 #include <iostream>
+#include <string>
 #include <ctime>
+#include <Windows.h>
+
+
 
 using namespace std;
 
@@ -11,18 +15,39 @@ int* randNumArray(int n) {
 	int* arr = new int[n];
 	srand(time(0));
 	for (int i = 0; i < n; i++) {
-		int temp = rand() % 30;                   // A function to determine the random starting positions of N stars
-		arr[i] = temp;				  // returns them as array of ints
+		int temp = rand() % 30;										// Function to create an array of random numebrs between 0 and 29
+		arr[i] = temp;												// Starting positons of every new set of stars
 	}
 	return arr;
+}
+
+void positionCursor(int x, int y) {
+	COORD cord;
+	cord.X = x;
+	cord.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), cord);  // Function to position the console curson on coords x y
+}
+
+void clearRow(int n) {
+	if (n < 0) return;
+	positionCursor(0, n);											//Function to completely clear row n
+	cout << string(80, ' ');
+}
+
+void resetConsole() {
+	positionCursor(0, 0);
+	for (int i = 0; i < 25; i++) {									//Function to clear the whole console
+		cout << string(80, ' ');
+	}
+	positionCursor(0, 0);
 }
 
 void printALine(int* src, int n) {
 	for (int i = 0; i < n; i++) {
 		int curr = src[i];
 		for (int i = 0; i < curr; i++) {
-			cout << " ";                       // A function to print a sing line where on every position of the array
-		}					   // there's a * symbol
+			cout << " ";										//Function to print one line with stars
+		}
 		cout << "*";
 	}
 	cout << endl;
@@ -31,11 +56,21 @@ void printALine(int* src, int n) {
 void print(int n) {
 	while (true) {
 		int* arr = randNumArray(n);
-		for (int i = -6; i < 25; i++) {            //function to run the simulation. Very flawed
-			printALine(arr, n);
+		int real;
+		srand(time(0));
+		int die = rand() % 19;
+		for (int i = -6; i < die; i++) {
+			real = i + 6;
+			printALine(arr, n);									//the driver fucntion
+			clearRow(i);
+			positionCursor(0, real);
+			Sleep(100);
 		}
+		resetConsole();
 	}
 }
+
+
 
 
 int main()
